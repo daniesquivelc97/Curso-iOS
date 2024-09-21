307f8ad3c7eb2250c620aef57af6fd4610233d25
Daniel Alejandro Esquivel Correa <daniel.esquivel.-nd@disney.com>
2024-09-21 12:56:57 -0500

    IMC app finished

---
+++ IMC/IMCResult.swift
@@ -0,0 +1,18 @@
+//
+//  IMCResult.swift
+//  CursoiOS
+//
+//  Created by Daniel Alejandro Esquivel Correa on 21/09/24.
+//
+
+import SwiftUI
+
+struct IMCResult: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IMCResult()
+}
---
+++ components/ListExample.swift
@@ -0,0 +1,18 @@
+//
+//  ListExample.swift
+//  CursoiOS
+//
+//  Created by Daniel Alejandro Esquivel Correa on 21/09/24.
+//
+
+import SwiftUI
+
+struct ListExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListExample()
+}
